a29f71184540823fd3b7c8cc04c4f359abeac088
dx02305@gmail.com <dx02305@gmail.com>
2023-02-09 18:13:11 +0100

    mate

--- autoinstall.sh
+++ autoinstall.sh
@@ -69,7 +69,7 @@ sudo apt install -y expect
 
 cat <<EOF | expect
 set timeout -1
-spawn sudo apt install -y ubuntu-mate-desktop
+sudo apt install -y ubuntu-mate-desktop
 expect "Default display manager: "
 send "lightdm\n"
 expect eof
