6a476973c0f4f24de03fc84510eed1a4f9e96999
david <dx02305@gmail.com>
2023-02-09 15:59:46 +0100

    init

---
+++ autoinstall.sh
@@ -0,0 +1,12 @@
+# update
+sudo apt update
+
+# install vscode
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+# install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
