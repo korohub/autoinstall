861d638edae3d707f2e624033db66670c3522f3a
dx02305@gmail.com <dx02305@gmail.com>
2023-02-09 16:30:41 +0100

    add  filezilla

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,8 +5,17 @@ sudo apt update
 sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+sudo apt install code -y
 
 # install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# putty
+sudo apt install putty -y
+
+# filezilla
+sudo apt install filezilla -y
+
+# dbeaver
+sudo apt install dbeaver-ce -y
