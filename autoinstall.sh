a09613715eb710aabc8e04b74fede0efb0ac9ba3
dx02305@gmail.com <dx02305@gmail.com>
2023-02-09 16:52:44 +0100

    source

--- autoinstall.sh
+++ autoinstall.sh
@@ -8,6 +8,7 @@ sudo apt install net-tools lsb-release
 echo "alias 'p=python3'" >> ~/.bashrc
 echo "alias 'update=sudo apt update'" >> ~/.bashrc
 
+source ~/.bashrc 
 
 # install vscode
 sudo apt install software-properties-common apt-transport-https wget -y
